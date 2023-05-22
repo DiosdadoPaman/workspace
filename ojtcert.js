df7729439fd8873c5a41c8ba523a8583fd5aa437
Jayar <89890358+DiosdadoPaman@users.noreply.github.com>
2023-05-22 15:54:19 +0800

    Main

---
+++ ojtcert.js
@@ -0,0 +1,7 @@
+    //Typing Text OJT Certificate
+    var typed = new Typed('.my_ojtcert', {
+        strings: ['OJT Certificate'],
+        typeSpeed:140,
+        backSpeed:140,
+        loop:true,
+      }); 
